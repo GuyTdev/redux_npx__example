fb64371154348d7745b71decfe80a30e023c841b
Guy T Dev <guytdev@gmail.com>
2022-11-16 19:04:44 +0200

    redux built in example

--- src/features/counter/counterAPI.js
+++ src/features/counter/counterAPI.js
@@ -1,6 +1,7 @@
 // A mock function to mimic making an async request for data
 export function fetchCount(amount = 1) {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ data: amount }), 500)
+  return new Promise((resolve, reject) => amount>0?
+    setTimeout(() => resolve({ data: amount }), 500):
+    reject(console.log("amount is negetive"))
   );
 }
